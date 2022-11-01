96d421b6aa8425a1f97fc63201d6c2320f065be0
Takashi Mori <kanetugu2015@gmail.com>
2022-11-01 17:38:37 +0900

    update for windows build

--- src/hako/utils/hako_logger.cpp
+++ src/hako/utils/hako_logger.cpp
@@ -6,7 +6,13 @@ void hako::utils::logger::init(const std::string &id)
 {
     char logdir_path[4096];
     sprintf(logdir_path, "%s/", HAKO_LOGGER_DIRPATH);
+#ifdef WIN32 
+    wchar_t win_logdir_path[4096];
+    mbstowcs(win_logdir_path, logdir_path, sizeof(logdir_path));
+    (void)_wmkdir(win_logdir_path);
+#else
     (void)mkdir(logdir_path, 0777);
+#endif
     std::string logfile_path = logdir_path + id + HAKO_LOGGER_FILE_EXTENSION;
     spdlog::basic_logger_mt(id, logfile_path);
     spdlog::flush_every(std::chrono::seconds(1));
