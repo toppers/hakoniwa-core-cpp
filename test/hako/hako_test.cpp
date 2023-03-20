46be70e72695f720050c1b47971a75b1a620e47b
森崇 <kanetugu2015@gmail.com>
2023-03-20 14:09:35 +0900

    add logical channel test

--- test/hako/hako_test.cpp
+++ test/hako/hako_test.cpp
@@ -64,3 +64,52 @@ TEST_F(HakoTest, IHakoAssetController_01)
     //done
     hako::destroy();
 }
+
+TEST_F(HakoTest, IHakoAssetController_02)
+{
+    //prepare
+    hako::init();
+
+    HakoTimeType max_delay_usec = 100000ULL;
+    HakoTimeType delta_usec = 10000ULL;
+    std::shared_ptr<hako::IHakoMasterController> hako_master = hako::create_master();
+    hako_master->set_config_simtime(max_delay_usec, delta_usec);
+
+    //do
+    hako_asset = hako::create_asset_controller();
+
+    AssetCallbackType callback;
+    callback.reset = nullptr;
+    callback.start = nullptr;
+    callback.stop = nullptr;
+    auto ret = hako_asset->asset_register("TestAsset_01", callback);
+    EXPECT_TRUE(ret);
+    ret = hako_asset->asset_register("TestAsset_02", callback);
+    EXPECT_TRUE(ret);
+
+    ret = hako_asset->create_pdu_lchannel("TestAsset_01", 0, 256);
+    EXPECT_TRUE(ret);
+    ret = hako_asset->create_pdu_lchannel("TestAsset_01", 1, 256);
+    EXPECT_TRUE(ret);
+    ret = hako_asset->create_pdu_lchannel("TestAsset_02", 0, 256);
+    EXPECT_TRUE(ret);
+    ret = hako_asset->create_pdu_lchannel("TestAsset_02", 1, 256);
+    EXPECT_TRUE(ret);
+
+    auto real_cid = hako_asset->get_pdu_channel("TestAsset_01", 0);
+    EXPECT_EQ(real_cid, 0);
+    real_cid = hako_asset->get_pdu_channel("TestAsset_01", 1);
+    EXPECT_EQ(real_cid, 1);
+    real_cid = hako_asset->get_pdu_channel("TestAsset_02", 0);
+    EXPECT_EQ(real_cid, 2);
+    real_cid = hako_asset->get_pdu_channel("TestAsset_02", 1);
+    EXPECT_EQ(real_cid, 3);
+
+    ret = hako_asset->asset_unregister("TestAsset_01");
+    EXPECT_TRUE(ret);
+    ret = hako_asset->asset_unregister("TestAsset_02");
+    EXPECT_TRUE(ret);
+
+    //done
+    hako::destroy();
+}
